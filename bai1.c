ce4fcfd11bddc5d9fac56abf41183ffbd119d82d
bathai <nb.thai2223712001@gmail.com>
2022-04-04 02:27:15 +0700

    end

--- bai1.c
+++ bai1.c
@@ -4,29 +4,59 @@
 #include <ctype.h>
 typedef struct node
 {
-    char *w;
-    int *a;
+    char *s;
+    int a[20];
+    int size;
     struct node *next;
 }node;
-node *createNode(char x[], int i)
+node *createNode(char *c, int i)
 {
     node *new = malloc(sizeof(struct node));
     new -> next = NULL;
-    new -> w = malloc(sizeof(strlen(x)));
-    strcpy( new -> w, x);
+    new -> s = malloc(sizeof(strlen(c)));
+    strcpy( new -> s, c);
     new -> a[0] = i;
+    new -> size = 1;
     return new;
 } 
-
-// void addAd(node *n, int i)
-// {
-//     int j = 0;
-//     while(n -> a[j] != NULL)
-//     {
-//         j++;
-//     }
-//     n -> a[j] = i;
-// }
+void addAd(node *n, int i)
+{
+    n -> a[n -> size] = i;
+    (n -> size) ++;
+}
+void sort(node **head, char * s, int line)
+{
+    if (*head == NULL)
+    {
+        *head = createNode( s, line);
+    }
+    else
+    {   
+        node *p = *head;
+        if((strcmp( p->s, s) > 0))
+        { 
+            node *q = createNode( s, line);
+            q -> next = p;
+            *head = q;
+        }
+        else
+        {    
+            do{
+                if(strcmp( p->s, s) == 0) 
+                {
+                    addAd(p, line);
+                    return;
+                }
+                if( p -> next == NULL)break;
+                if(strcmp( p->next->s, s) <= 0)p = p->next;
+                else break;
+            }while(1);
+            node *q = createNode( s, line);
+            q -> next = p -> next;
+            p -> next = q;
+        }
+    } 
+}
 char ** readstopw(char *filename, int * size)
 {
     FILE *f = fopen(filename,"r");
@@ -43,9 +73,8 @@ char ** readstopw(char *filename, int * size)
     return str;
 }
 
-char * process(char * s, int *b, int *t)
+char * process(char * s, int *b)
 {   
-    printf("\nbefore: %s %d",s,*b);
     int i;
     for(i = 0; i < strlen(s); i++)
     {
@@ -57,24 +86,12 @@ char * process(char * s, int *b, int *t)
     }
     else 
     {
-        if(s[strlen(s) -1] == '\n')
-        {
-            (*t) ++;
-        }
         return NULL;
     }
     if('A' <= s[0] && s[0] <= 'Z') // kiểm tra tên riêng và xử lý chữ hoa về chữ thường
     {
         if((*b) == 0) //b = 1 co dau cham, b = 0 ko co dau cham
         {
-            if(s[strlen(s) - 1] == '\n')
-            {
-                (*t) ++;
-                if(s[strlen(s) - 2] == '.' ||s[strlen(s) - 2] == '?')
-                {
-                    (*b) = 1;
-                }
-            }
             if(s[strlen(s) - 1] == '.'|| s[strlen(s) - 1] == '?')
             {
                 (*b) = 1;
@@ -88,18 +105,14 @@ char * process(char * s, int *b, int *t)
     }
     if(!isalpha(s[strlen(s) - 1]))
     {
-        if(s[strlen(s) - 1] == '\n')
-        {
-            (*t) ++;
-            if(s[strlen(s) - 2] == '.' ||s[strlen(s) - 2] == '?')
-            {
-                (*b) = 1;
-            }
-        }
         if(s[strlen(s) - 1] == '.'|| s[strlen(s) - 1] == '?')
         {
             (*b) = 1;
         }
+        do
+        {
+            s[strlen(s) - 1] = '\0';
+        }while(!isalpha(s[strlen(s) - 1]));
     }
     else
     {
@@ -111,19 +124,28 @@ char * process(char * s, int *b, int *t)
         {
             s[i] = tolower(s[i]);
         }
-
     }
-    
-    printf("\n\n b: %d",*b);
     return s;
 }
-
-void solve(char * filename,char ** str,int size)
+void display(node *head){
+    node *p = head;
+    while(p != NULL)
+    {
+        printf("\n%s ",p->s);
+        for(int i = 0; i < p -> size - 1; i++)
+        {
+            printf("%d,",p->a[i]);
+        }
+        printf("%d",p->a[p -> size - 1]);
+        p = p->next;
+    }
+}
+node * solve(char * filename,char ** str,int size)
 {
     FILE *f = fopen(filename,"r");
     if( f == NULL) printf("\nCannot read file %s",filename);
     char *s;
-    node *n;
+    node *n = NULL;
     int t = 1;
     int b = 1;
     int c = 0;
@@ -131,38 +153,44 @@ void solve(char * filename,char ** str,int size)
     char *d;
     while(fscanf(f,"%s",temp) == 1)
     {
-                 
-            d = malloc(sizeof(temp));
-            strcpy(d,temp);
-
-            d = process(d, &b, &t);
-
-            if(d == NULL)
+        char k = getc(f);
+        if(k == ' ') 
+        {
+            k = getc(f);
+            if(isalpha(k))
             {
-                continue;
+                fseek( f, -1, SEEK_CUR );
             }
-            for(int i = 0; i < size; i++)
-            {
-                if(strcmp(d,str[i]) == 0)
-                {
-                    c = 1;
-                    break;
-                }
-            }
+            if(k == '\n')t++;
+        }
+        else if(k == '\n')t++;
+        d = malloc(sizeof(temp));
+        strcpy(d,temp);
+        d = process(d, &b);
 
-            if(c == 0) printf("\n%s",d);
-            c = 0;
-        
+        if(d == NULL)
+        {
+            continue;
+        }
+        for(int i = 0; i < size; i++)
+        {
+            if(strcmp(d,str[i]) == 0)
+            {
+                c = 1;
+                break;
+            }
+        }
+        if(c == 0)sort(&n,d,t);
+        else c = 0;
     }
+    return n;
 }
-void main()
+
+int main()
 {
     int size;
     char **str = readstopw("stopw.txt", &size);
-    // solve("vanban.txt",str,size);
-    char s[] = "adf.\n";
-    int a = 0,b = 0;
-    char *st = process(s,&a,&b);
-    printf("\n%s %d %d",st,a,b);
-    // printf("\n%s",d);
+    node *n = solve("vanban.txt",str,size);
+    display(n);
+    return 1;
 }
